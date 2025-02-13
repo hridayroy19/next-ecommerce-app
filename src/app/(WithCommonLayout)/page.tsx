8cab39e4825f12cecc09d2a221570e45f752b137
hridayroy19 <hrhridoyroy503@gmail.com>
2025-02-13 11:07:58 +0600

    projcet setupd and creat folter stucture and navbar footer

---
+++ src/app/(WithCommonLayout)/page.tsx
@@ -0,0 +1,15 @@
+const Homepage = () => {
+  return (
+    <div className=" mt-20 px-16">
+      <button className="bg-primary text-primary-foreground px-4 py-2 rounded-md hover:bg-opacity-80 transition">
+        Shop Now
+      </button>
+
+      <div className="bg-background text-foreground p-6 rounded-lg ">
+        Welcome to our store!
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
