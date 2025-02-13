ecc89df1558aff9256e13cb5f971d317ffdf8bb1
hridayroy19 <hrhridoyroy503@gmail.com>
2025-02-13 11:56:25 +0600

    create register formm

--- src/components/modules/auth/register/RegisterForm.tsx
+++ src/components/modules/auth/register/RegisterForm.tsx
@@ -9,17 +9,31 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { registrationSchema } from "./registerValidation";
+import Image from "next/image";
 
 const RegisterForm = () => {
-  const form = useForm();
+  const form = useForm({
+    resolver: zodResolver(registrationSchema),
+  });
 
-  const onSubmit = (data) => {
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
     console.log(data);
   };
 
   return (
-    <div>
+    <div className="border-2 border-gray-300 rounded-xl flex-grow max-w-md w-full p-5">
+      <div className="flex items-center space-x-4 ">
+        <Image src="/assert/logo.png" alt="logo" width={200} height={200} />
+        <div>
+          <h1 className="text-xl font-semibold">Register</h1>
+          <p className="font-extralight text-sm text-gray-600">
+            Join us today and start your journey!
+          </p>
+        </div>
+      </div>
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)}>
           <FormField
@@ -27,6 +41,7 @@ const RegisterForm = () => {
             name="name"
             render={({ field }) => (
               <FormItem>
+                <FormLabel>Name</FormLabel>
                 <FormLabel />
                 <FormControl>
                   <Input {...field} value={field.value || ""} />
@@ -35,7 +50,52 @@ const RegisterForm = () => {
               </FormItem>
             )}
           />
-          <Button type="submit"> Register </Button>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormLabel />
+                <FormControl>
+                  <Input {...field} value={field.value || ""} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormLabel />
+                <FormControl>
+                  <Input {...field} value={field.value || ""} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="passwordConfirm"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel> Confarm Password</FormLabel>
+                <FormLabel />
+                <FormControl>
+                  <Input {...field} value={field.value || ""} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button className="mt-5" type="submit">
+            {" "}
+            Register{" "}
+          </Button>
         </form>
       </Form>
     </div>
