c7fbb5b44ed42923a2e913ad00433f2870bc9d9f
hridayroy19 <hrhridoyroy503@gmail.com>
2025-02-14 15:08:02 +0600

    creat login page and user login successfuly

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,11 @@
+import LoginForm from "@/components/modules/auth/login/LoginForm";
 
 const Loginpage = () => {
-    return (
-        <div>
-            login
-        </div>
-    );
+  return (
+    <div className="h-screen w-screen items-center flex justify-center">
+      <LoginForm />
+    </div>
+  );
 };
 
 export default Loginpage;
--- src/components/modules/auth/login/LoginForm.tsx
+++ src/components/modules/auth/login/LoginForm.tsx
@@ -11,31 +11,24 @@ import {
 import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
-import { registrationSchema } from "./registerValidation";
 import Image from "next/image";
-import { registerUser } from "@/services/AuthService";
+import { loginUser } from "@/services/AuthService";
 import { toast } from "sonner";
+import { loginSchema } from "./loginValidation";
 import Link from "next/link";
 
-const RegisterForm = () => {
+const LoginForm = () => {
   const form = useForm({
-    resolver: zodResolver(registrationSchema),
+    resolver: zodResolver(loginSchema),
   });
-
-  const {
-    formState: { isSubmitting },
-  } = form;
-
-  // password validation
-  const password = form.watch("password");
-  const passwordConfirm = form.watch("passwordConfirm");
-  // console.log(password, passwordConfirm);
+    
+  const {formState:{isSubmitting}} = form
 
   const onSubmit: SubmitHandler<FieldValues> = async (data) => {
     try {
-      const res = await registerUser(data);
+      const res = await loginUser(data);
       console.log(res);
-
+      
       if (res?.success) {
         toast.success(res?.message);
       } else {
@@ -51,28 +44,14 @@ const RegisterForm = () => {
       <div className="flex items-center space-x-4 ">
         <Image src="/assert/logo.png" alt="logo" width={200} height={200} />
         <div>
-          <h1 className="text-xl font-semibold">Register</h1>
+          <h1 className="text-xl font-semibold">Login</h1>
           <p className="font-extralight text-sm text-gray-600">
-            Join us today and start your journey!
+            Wellcome come Back
           </p>
         </div>
       </div>
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)}>
-          <FormField
-            control={form.control}
-            name="name"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Name</FormLabel>
-                <FormLabel />
-                <FormControl>
-                  <Input type="text" {...field} value={field.value || ""} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
           <FormField
             control={form.control}
             name="email"
@@ -101,45 +80,23 @@ const RegisterForm = () => {
               </FormItem>
             )}
           />
-          <FormField
-            control={form.control}
-            name="passwordConfirm"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel> Confarm Password</FormLabel>
-                <FormLabel />
-                <FormControl>
-                  <Input type="password" {...field} value={field.value || ""} />
-                </FormControl>
-                {passwordConfirm && password !== passwordConfirm ? (
-                  <FormMessage className="text-red-600">
-                    {" "}
-                    password dose not match
-                  </FormMessage>
-                ) : (
-                  <FormMessage />
-                )}
-                <FormMessage />
-              </FormItem>
-            )}
-          />
           <Button
-            disabled={passwordConfirm && password !== passwordConfirm}
             className="mt-5 w-full"
             type="submit"
           >
-            {isSubmitting ? "Registaring...." : "Register"}
+            {isSubmitting ? "Login...." : "Login"}
           </Button>
         </form>
       </Form>
       <p className="text-sm text-gray-600 text-center my-3">
-        Already have an account ?
-        <Link href="/login" className="text-primary">
-          Login
+        Do not have any account ?
+        <Link href="/register" className="text-primary">
+          Register
         </Link>
       </p>
+        <p/>
     </div>
   );
 };
 
-export default RegisterForm;
+export default LoginForm;
