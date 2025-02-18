441b86d5ac023d2bf0cca534e1b06e75a84d6fa1
hridayroy19 <hrhridoyroy503@gmail.com>
2025-02-18 11:57:33 +0600

    crate product and all product show mange section

---
+++ src/app/(WithDashboardLayout)/user/shop/products/page.tsx
@@ -0,0 +1,15 @@
+import ManageProducts from "@/components/modules/shop/product";
+import { getAllProducts } from "@/services/Product";
+
+
+const ManageProductsPage = async () => {
+  const { data, meta } = await getAllProducts();
+  console.log(data)
+  return (
+    <div>
+      <ManageProducts products={data}/>
+    </div>
+  );
+};
+
+export default ManageProductsPage;
