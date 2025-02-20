1a8f592ef7417dbdd3c297d60b36569db0990b2a
hridayroy19 <hrhridoyroy503@gmail.com>
2025-02-20 10:58:46 +0600

    crete hero section

--- src/app/(WithCommonLayout)/(home)/page.tsx
+++ src/app/(WithCommonLayout)/(home)/page.tsx
@@ -1,13 +1,9 @@
-"use client";
-import { useUser } from "@/context/UserContext";
+import HeroSection from "@/components/modules/home/Herosection";
 
 const Homepage = () => {
-  const user = useUser();
-  console.log(user);
-
   return (
     <div className="bg-background text-foreground p-6 rounded-lg ">
-      Welcome to our store!
+      <HeroSection />
     </div>
   );
 };
