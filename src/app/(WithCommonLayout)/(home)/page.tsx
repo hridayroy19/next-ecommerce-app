9babaaad1f1c2fec9d44de1851a3097ce444894a
hridayroy19 <hrhridoyroy503@gmail.com>
2025-02-20 11:06:31 +0600

    create home section category section and dynamic data show

--- src/app/(WithCommonLayout)/(home)/page.tsx
+++ src/app/(WithCommonLayout)/(home)/page.tsx
@@ -1,9 +1,11 @@
+import Category from "@/components/modules/home/Category";
 import HeroSection from "@/components/modules/home/Herosection";
 
 const Homepage = () => {
   return (
     <div className="bg-background text-foreground p-6 rounded-lg ">
       <HeroSection />
+      <Category/>
     </div>
   );
 };
