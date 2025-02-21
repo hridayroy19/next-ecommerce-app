f82f893416d8f8731791ff804b87f6a0c5912047
hridayroy19 <hrhridoyroy503@gmail.com>
2025-02-21 17:40:47 +0600

    create cart page

--- src/app/(WithCommonLayout)/cart/page.tsx
+++ src/app/(WithCommonLayout)/cart/page.tsx
@@ -1,10 +1,17 @@
+import CartProducts from "@/components/modules/cart/CartProducts";
+import Coupon from "@/components/modules/cart/Coupon";
+import ProductBanner from "@/components/modules/products/banner";
 
-const page = () => {
-    return (
-        <div>
-            <h1> Cart page</h1>
-        </div>
-    );
+const CartPage = () => {
+  return (
+    <div className="container mx-auto my-10">
+      <ProductBanner title="Cart Page" path="Home - Cart" />
+      <div className="grid grid-cols-12 p-7 items-center justify-center gap-8 my-5">
+        <CartProducts />
+        <Coupon />
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default CartPage;
