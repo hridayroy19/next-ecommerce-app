9ac5d08733661f934ea75eea9d270aca824cb98d
hridayroy19 <hrhridoyroy503@gmail.com>
2025-02-23 13:16:28 +0600

    use readux and show cart page

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
+import cartSlice from "./features/cartSlice";
 
 export const makeStore = () => {
   return configureStore({
-    reducer: {},
+    reducer: {cartSlice},
   })
 }
 
